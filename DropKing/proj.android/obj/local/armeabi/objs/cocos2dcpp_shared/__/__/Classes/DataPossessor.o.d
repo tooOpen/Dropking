f7d90a624a4a9d4cdd1a8d86eb0db163ea92d82d
soonhang Byun <sabjil0608@naver.com>
2015-12-25 15:32:24 +0900

    delete old one

--- DropKing/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/DataPossessor.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/DataPossessor.o: \
- jni/../../Classes/DataPossessor.cpp jni/../../Classes/DataPossessor.h
-
-jni/../../Classes/DataPossessor.h:
